dbf8f1f1f745545481836ab3e15d4b25588b60f0
Chalermchon Samana <appcodev@gmail.com>
2013-08-19 00:51:52 +0700

    ok completed work

--- DemoSocialFramework1/DemoSocialFramework1/OZViewController.m
+++ DemoSocialFramework1/DemoSocialFramework1/OZViewController.m
@@ -8,7 +8,15 @@
 
 #import "OZViewController.h"
 
-@interface OZViewController ()
+//step 1 import Social.framework and Accounts.framework to project (for iOS 6.0 or lastest..)
+//step 2 code #import <Social/Social.h> in class viewcontroller
+#import <Social/Social.h>
+
+@interface OZViewController (){
+    
+    IBOutlet UITextView *sharedText;
+    IBOutlet UIImageView *sharedImage;
+}
 
 @end
 
@@ -24,6 +32,38 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//step 3 manage data to share such as text, image, url
+//step 4 code action to share
+///////////////// action ////////////////////
+
+- (IBAction)shareDefault:(UIButton *)sender {
+    NSArray *Items = @[sharedText.text,sharedImage.image];
+    
+    UIActivityViewController *activityView = [[UIActivityViewController alloc]
+                                              initWithActivityItems:Items
+                                              applicationActivities:nil];
+    
+    [self presentViewController:activityView animated:YES completion:nil];
+}
+
+- (IBAction)shareToFacebook:(id)sender {
+    SLComposeViewController *slView = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+    
+    [slView setInitialText:sharedText.text];
+    [slView addImage:sharedImage.image];
+    
+    [self presentViewController:slView animated:YES completion:nil];
+}
+
+- (IBAction)shareToTwitter:(id)sender {
+    SLComposeViewController *slView = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    
+    [slView setInitialText:sharedText.text];
+    [slView addImage:sharedImage.image];
+    
+    [self presentViewController:slView animated:YES completion:nil];
 }
 
 @end
