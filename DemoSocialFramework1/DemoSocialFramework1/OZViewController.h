5a8061341b9e81de7539fb58d09165d94abeb8b3
Chalermchon Samana <appcodev@gmail.com>
2013-08-19 00:31:03 +0700

    a begin code... to git

---
+++ DemoSocialFramework1/DemoSocialFramework1/OZViewController.h
@@ -0,0 +1,13 @@
+//
+//  OZViewController.h
+//  DemoSocialFramework1
+//
+//  Created by Chalermchon Samana on 8/19/13.
+//  Copyright (c) 2013 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZViewController : UIViewController
+
+@end
---
+++ DemoSocialFramework1/DemoSocialFramework1/OZViewController.m
@@ -0,0 +1,29 @@
+//
+//  OZViewController.m
+//  DemoSocialFramework1
+//
+//  Created by Chalermchon Samana on 8/19/13.
+//  Copyright (c) 2013 Onzondev Innovation Co., Ltd. All rights reserved.
+//
+
+#import "OZViewController.h"
+
+@interface OZViewController ()
+
+@end
+
+@implementation OZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
